d45c1abfa2e5482109b1d957fb8a5ab9e4fbeb4c
MichelleDuque <michelled_1994@hotmail.com>
2024-02-13 23:50:46 -0500

    added props and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import Heading from './Heading';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Main from './Main';
+import avatar from './avatar.webp'
+import Card from './Card';
 
+function Logo(props){
+  const logoAvatar ={
+    width:"100px",
+  }
 
+  const userPic = <img src={avatar} style={logoAvatar} className='logoAvatar' alt="Avatar"/>;
+  return userPic;
+}
 
 function App() {
   return( 
   <div className="App">
-  <Heading />
+  <Header firstName="Bob" />
+  <Logo />
+  <Header firstName="Gary" />
+  <Main greet="Howdy" />
+  <Sidebar greet="Hi" />
+  <h1>Task: Add three Card elements</h1>
+  <Card h2="First card's h2" h3="First card's h3" /> 
+  <Card h2="Second card's h2" h3="Second card's h3" /> 
+  <Card h2="Third card's h2" h3="Third card's h3" /> 
+
   </div>
   );
 }
